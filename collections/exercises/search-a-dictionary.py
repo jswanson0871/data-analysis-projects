ea0b1e12ef7dd1c5391d62a58f6b4872e198c10e
Jamesia Swanson <jhenderson0871@gmail.com>
2025-11-18 17:35:47 -0600

    done

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -12,21 +12,29 @@ flavors = {
 ## Set a variable called choice to the flavor you want to search for.
 
 ## Use an if statement to check if choice is in the flavors dictionary.
+choice="mint chocolate chip"
 
+if choice in flavors:
+    print (choice, "is in this list.")
 ## If it is, set another variable called cost to the value associated with choice.
-
+if choice in flavors:
+    cost=flavors[choice]
 ## If it isn’t, set cost to 0.
-
+else:
+    cost=0
 ## Print the cost.
-
+print ("The cost of flavor", choice, "is", cost)
 ### Search a Dictionary Part 2:
 
 ## Initialize two variables: highest_cost to 0 and fanciest to an empty string.
-
+highest_cost=0
+fanciest=""
 ## Loop through the flavors dictionary using a for loop.
-
+for flavor in flavors:
 ## For each flavor, check if its price is higher than highest_cost.
-
+  if flavors[flavor]>highest_cost:
 ## If it is, update fanciest to this flavor and highest_cost to its price.
-
+ fanciest=flavor
+    highest_cost=flavors[flavor]
 ## After the loop, print the most expensive flavor.
+print ("The most expensive flavor is", fanciest, ". It costs", highest_cost)
--- collections/exercises/strings.py
+++ collections/exercises/strings.py
@@ -4,22 +4,33 @@ text = 'Strings_are_sequences_of_characters.'
 word = 'tomato'
 
 # 1. Print a slice of the first 12 characters from 'text'.
-
+print(text[0:12])
 
 # 2. Print a slice of the last 12 characters from 'text'. You should NOT have to count the index values yourself!
-
+print(text[-12:])
 
 # 3. Print a slice of the middle 12 characters from 'text'.
-
+length_of_text=len(text)
+print(length_of_text//2-6)
+length_of_text=len(text)
+print(length_of_text//2-6)
 
 # ---- Exercise 3: Looping Through a String ----
 
 # Use index values to loop backwards through 'word'.
 
 # 1. Print 1 letter per line.
-
+for i in word:
+    print (i)
 
 # 2. Refactor the code to use the accumulator pattern to build up and print the reversed string. For example, if given 'good', print 'doog' on one line.
+max_index=len(word)-1
 
+new_word="" 
+for index in range(max_index, -1,-1):
+    new_word +=word[index]
+
+print(new_word)
 
 # 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'. (If you want to be fancy, print 'tomato | otamot').
+print(f"{word}|{new_word}")
